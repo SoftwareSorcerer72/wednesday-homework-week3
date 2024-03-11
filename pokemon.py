0784975304217b1697ef90e6d71db6051b00a805
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-11 07:13:29 -0400

    Tested the initial functionality with print statements.

--- pokemon.py
+++ pokemon.py
@@ -5,7 +5,11 @@ class PokemonAPI:
 
     def __get(self, endpoint, id_or_name):
         request_url = self.main_url + endpoint + '/' + id_or_name
-        return request_url
+        response = requests.get(request_url)
+        if response.ok:
+            return response.json()
+        else:
+            return None
     
 client = PokemonAPI()
 
