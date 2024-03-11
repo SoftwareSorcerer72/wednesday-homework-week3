d4b2b778886a11a8dd8130e516009e4b6de7e9d8
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-11 07:10:08 -0400

    Got the basic class set up.

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,11 @@
 import requests
 
-class PokeAPI:
+class PokemonAPI:
     main_url = "https://pokeapi.co/api/v2/"
 
+    def __get(self, endpoint, id_or_name):
+        request_url = self.main_url + endpoint + '/' + id_or_name
+        return request_url
     
+client = PokemonAPI()
+
